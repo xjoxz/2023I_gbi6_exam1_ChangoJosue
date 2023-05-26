97c1be8625ec176085f8cb87bff93231e4e9d7c3
xjoxz <104948498+xjoxz@users.noreply.github.com>
2023-05-25 21:41:39 -0500

    Resolución pregunta 3

--- mirbase.sh
+++ mirbase.sh
@@ -1,8 +1,8 @@
 #!bin/bash 
 #Resolucion pregunta #3 
 
-egrep "PUBMED" miRNA.dat | wc -l 
+egrep "PUBMED" miRNA.dat | wc -l >> elegans.txt
 
-egrep "Nature" miRNA.dat -n1 | egrep "elegans" | wc -l 
+egrep "Nature" miRNA.dat -n1 | egrep "elegans" | wc -l >> elegans.txt 
 
-egrep "139 BP" miRNA.dat | wc -l 
+egrep "139 BP" miRNA.dat | wc -l >> elegans.txt
